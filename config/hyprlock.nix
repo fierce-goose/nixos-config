38f233fc1f71a119e59f46df4e1772911a6987ec
fierce-goose <fierce.goose90053@gmail.com>
2024-10-20 18:50:04 -0700

    updated

--- config/hyprlock.nix
+++ config/hyprlock.nix
@@ -16,14 +16,14 @@
       }];
       input-field = [{
         size = "1920, 100";
-        position = "0, -250";
+        position = "0, -200";
         dots_center = true;
         inner_color = "rgba(14, 25, 22, 0.6)";
         font_color = "rgba(234, 242, 229, 0.85)";
         outline_thickness = 0;
         fade_on_empty = false;
         rounding = 0;
-        placeholder_text = "<span foreground=\"##eaf2e5d9\">Input Password...</span>";
+        placeholder_text = "<span foreground=\"##eaf2e592\">Input Password...</span>";
         fail_text = "<span foreground=\"##eaf2e5d9\"><i>$FAIL <b>($ATTEMPTS)</b></i></span>";
       }];
       label = [
@@ -31,7 +31,6 @@
           text = "$TIME";
           color = "rgba(234, 242, 229, 0.9)";
           font_size = 128;
-          font_family = "Sans";
           position = "0, 0";
           halign = "center";
           valign = "center";
@@ -43,8 +42,7 @@
           text = "Current layout: $LAYOUT";
           color = "rgba(14, 25, 22, 0.7)";
           font_size = 24;
-          font_family = "Sans";
-          position = "30, -30";
+          position = "30, -22";
           halign = "left";
           valign = "top";
         }
